8d1a78d14966f3fc13fdeb3032309cb702762073
Marco Miranda <marmirand@hotmail.com>
2019-04-12 12:37:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-  animal = {"Panama" => "re-footed tortoise"}
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |arg|
+        if(value == arg)
+          array << key
+        end
+      end
+    end
+    array
   end
 end
